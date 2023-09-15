902812e3c24907db0e6b6f89976d63cd3d46fb4e
yassinelahs <lahsnaouiyassine1998@gmail.com>
2023-09-15 11:20:10 -0700

    0x00. C - Hello, World

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'puts out put '
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar\, but the outcome is a piece of art\,");
+	return (0);
+}
+
