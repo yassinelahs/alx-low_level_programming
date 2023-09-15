3daf2696ad296e2c48742c30a0fc66731e5ed90a
yassinelahs <lahsnaouiyassine1998@gmail.com>
2023-09-15 11:56:32 -0700

    0x00. C - Hello, World

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'puts out put '
+ * Return: Always 1 (Success)
+ */
+int main(void)
+{
+	char quote[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(1,quote,sizeof(quote));
+	return (1);
+}
+
