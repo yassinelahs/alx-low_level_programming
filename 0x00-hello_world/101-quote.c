1685d503d61a7b9d1e0dfdb71f5c49fe00ca21a7
yassinelahs <lahsnaouiyassine1998@gmail.com>
2023-09-15 12:01:39 -0700

    0x00. C - Hello, World

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,8 +7,9 @@
  */
 int main(void)
 {
-	char quote[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	write(1,quote,sizeof(quote));
+	char quote[] = "and that piece of art is useful\"- Dora Korpar, 2015-10-19\n";
+
+	write(1, quote, sizeof(quote));
 	return (1);
 }
 
