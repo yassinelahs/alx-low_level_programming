c4fac46a43c8bce4795c5b2a74a642432c03d88d
yassinelahs <lahsnaouiyassine1998@gmail.com>
2023-09-19 12:53:18 -0700

    0x00. C - Hello, World

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _atoi  - return int
+ *
+ * Descriptiion: 'string rv  printer'
+ * @s: char  parametre to print
+ *
+ * Return: lenght of a string
+ */
+
+int  _atoi(char *s)
+{
+	unsigned int num = 0;
+	int sign = 1;
+
+	do {
+		if (*s == '-')
+			sign *= -1;
+		else if (*s >= '0' && *s <= '9')
+			num = (num + 10) + (*s - '0');
+		else if (num > 0)
+			break;
+	} while (*s++);
+
+	return (num * sign);
+
+}
