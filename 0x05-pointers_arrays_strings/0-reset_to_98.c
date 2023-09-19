afd50e2c045089edad423c9362a93bb558d89c1a
yassinelahs <lahsnaouiyassine1998@gmail.com>
2023-09-18 18:37:56 -0700

    0x00. C - Hello, World

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * 0-reset_to_98.c
+ *
+ * Description: ' takes a pointer to an int as parameter and updates the value it points to to 98'
+ * @n: int parameter
+ *
+ * Return: nothing
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
