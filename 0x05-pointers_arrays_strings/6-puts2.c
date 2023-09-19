dec072f323d432dbff1cb7d70504f6457e28bb78
yassinelahs <lahsnaouiyassine1998@gmail.com>
2023-09-18 19:53:37 -0700

    0x00. C - Hello, World

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * puts2  - print a string rv
+ *
+ * Description: 'string rv  printer'
+ * @s: string parametre to print
+ *
+ * Return: lenght of a string
+ */
+void puts2(char *s)
+{
+	int i;
+
+	for (i = 0 ; s[i] != '\0'; ++i)
+	{
+		if (i % 2 == 0)
+			_putchar(str[i]);
+	}
+	_putchar('\n');
+
+}
