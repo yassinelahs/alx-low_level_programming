e7b1dec339ac6b42d4eb77afbf859d4eba411383
yassinelahs <lahsnaouiyassine1998@gmail.com>
2023-09-18 19:42:14 -0700

    0x00. C - Hello, World

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * rev_string  - print a string rv
+ *
+ * Description: 'string rv  printer'
+ * @s: string parametre to print
+ *
+ * Return: lenght of a string
+ */
+void rev_string(char *s)
+{
+	int l, i;
+	char temp;
+
+	for (l = 0; s[l] != '\0'; ++i)
+		;
+
+	for (i = 0 ; i < l / 2; i++)
+	{
+		temp = s[i];
+		s[i] = s[l - 1 - i];
+		s[l - 1 - i] = temp;
+	}
+
+}
