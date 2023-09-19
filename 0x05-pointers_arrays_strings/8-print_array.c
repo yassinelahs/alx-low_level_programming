fe654c1c6e503d001003eb0fe2cc654eb5b66e7b
yassinelahs <lahsnaouiyassine1998@gmail.com>
2023-09-18 20:13:01 -0700

    0x00. C - Hello, World

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_array  - print a string rv
+ *
+ * Description: 'string rv  printer'
+ * @a: int  parametre to print
+ * @n: int parametre
+ *
+ * Return: lenght of a string
+ */
+void print_array(int a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		if (i != (n - 1))
+			printf("%d, ", a[i]);
+		else
+			printf("%d", a[i]);
+	}
+	printf("\n");
+
+}
