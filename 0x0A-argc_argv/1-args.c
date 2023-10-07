1a5490c33cd6270380ac45423333eb236394f3b0
yassinelahs <lahsnaouiyassine1998@gmail.com>
2023-10-07 15:42:36 -0700

    0x00. C - Hello, World

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - print program name
+ * Description: 'this program is about print program name'
+ * @argc: int parameter
+ * @argv: char const parameter
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc , char *argv[])
+{
+	printf("%d\n", *argc);
+
+	return (0);
+}
